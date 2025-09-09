399a99db8fa0aab2b8624a95cd182d31def8f0a9
RatanSingh21 <ratan.singh@hdfclife.com>
2025-09-09 17:12:29 +0530

    Unversioned files done

---
+++ DAY 17 [Spring Security]/Assignment-LMS/src/main/java/in/ratansgh/Assignment_LMS/AssignmentLmsApplication.java
@@ -0,0 +1,13 @@
+package in.ratansgh.Assignment_LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentLmsApplication.class, args);
+	}
+
+}
