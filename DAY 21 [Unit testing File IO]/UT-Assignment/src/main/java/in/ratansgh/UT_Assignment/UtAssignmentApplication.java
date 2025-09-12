44b399e92169b5a9ce67ef275a346270cbd01386
RatanSingh21 <ratan.singh@hdfclife.com>
2025-09-12 23:33:10 +0530

    files added

---
+++ DAY 21 [Unit testing File IO]/UT-Assignment/src/main/java/in/ratansgh/UT_Assignment/UtAssignmentApplication.java
@@ -0,0 +1,13 @@
+package in.ratansgh.UT_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtAssignmentApplication.class, args);
+	}
+
+}
---
+++ DAY 21 [Unit testing File IO]/UT-Assignment/src/test/java/in/ratansgh/UT_Assignment/UtAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ratansgh.UT_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
