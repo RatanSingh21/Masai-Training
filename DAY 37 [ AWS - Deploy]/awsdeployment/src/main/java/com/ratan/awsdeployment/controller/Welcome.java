71cc64c95b35ecc96a35665dadba00c5bf42b01c
RatanSingh21 <ratan.singh@hdfclife.com>
2025-11-07 15:17:53 +0530

    AWS Backend Deployed

---
+++ DAY 37 [ AWS - Deploy]/awsdeployment/src/main/java/com/ratan/awsdeployment/controller/Welcome.java
@@ -0,0 +1,15 @@
+package com.ratan.awsdeployment.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the page!";
+    }
+
+}
