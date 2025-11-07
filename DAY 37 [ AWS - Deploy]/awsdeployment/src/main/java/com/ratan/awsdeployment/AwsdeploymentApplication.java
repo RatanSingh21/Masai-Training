ecc713c58160ee38b2a284e12b3d2a1670535f1e
RatanSingh21 <ratan.singh@hdfclife.com>
2025-11-07 12:39:47 +0530

    all directory up to mark

---
+++ DAY 37 [ AWS - Deploy]/awsdeployment/src/main/java/com/ratan/awsdeployment/AwsdeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.ratan.awsdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsdeploymentApplication.class, args);
+	}
+
+}
