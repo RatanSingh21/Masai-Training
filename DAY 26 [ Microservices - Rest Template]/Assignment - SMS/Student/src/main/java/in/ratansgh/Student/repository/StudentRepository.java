4d2e096ad1bbfc33ae78dbe36621213b918e40f8
RatanSingh21 <ratan.singh@hdfclife.com>
2025-11-10 12:29:20 +0530

    Rename

--- DAY 26 [ Microservices - Rest Template]/Assignment - SMS/Student/src/main/java/in/ratansgh/Student/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package in.ratansgh.Student.repository;
-
-import in.ratansgh.Student.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
